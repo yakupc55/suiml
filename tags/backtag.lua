2f9136f0b449258ce6bb3d1b693b6ffe8511b5a1
MSI\mypcc <yakupceran00@gmail.com>
2025-09-15 16:27:27 +0200

    small changes

--- tags/backtag.lua
+++ tags/backtag.lua
@@ -2,10 +2,13 @@ local ltag = {}
 local focus = require("focus")
 local template = require("template")
 function ltag.render(node,renderer)
+    print(node.attributes["style"])
+    print("st len: ",#node.style)
     local w, h = love.graphics.getDimensions()
     -- print("dimensions : w: ",w," h:",h)
-    if node.style.backcolor then
-        love.graphics.setColor(node.style.backcolor)
+    if node.style["backgroundColor"] then
+        print("background is goed")
+        love.graphics.setColor(node.style["backgroundColor"])
         love.graphics.rectangle("fill", 0, 0, w, h)
     end
 end
