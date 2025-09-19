2a2c35b528a59038a01506ff0abd75e74c2a7d32
Diyar Ceran <yakupceran00@gmail.com>
2025-09-20 01:58:42 +0200

    mini games changes: choose symbol list

--- tags/iftag.lua
+++ tags/iftag.lua
@@ -3,23 +3,26 @@ local focus = require("focus")
 local template = require("template")
 function iftag.render(node,renderer)
     local ok = false
-        if node.attributes["condition"] then
-            ok = _G[node.attributes["condition"]] == true
+    local result = false
+    local condition = template.renderAction(renderer,node.attributes["condition"])
+    local fn, err = load("return " .. condition)
+    if condition then
+        ok, result = pcall(fn)
+    end
+    if (ok and result) or false then
+        for _, child in ipairs(node.nodes) do
+            y = renderer.renderNode(child, x, y)
         end
-        if ok then
-            for _, child in ipairs(node.nodes) do
-                y = renderer.renderNode(child, x, y)
-            end
-        else
-            -- check for else
-            for _, child in ipairs(node.nodes) do
-                if child.name == "else" then
-                    for _, c in ipairs(child.nodes) do
-                        y = renderer.renderNode(c, x, y)
-                    end
+    else
+        -- check for else
+        for _, child in ipairs(node.nodes) do
+            if child.name == "else" then
+                for _, c in ipairs(child.nodes) do
+                    y = renderer.renderNode(c, x, y)
                 end
             end
         end
+    end
 end
 
 return iftag
